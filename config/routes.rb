7e453d7a4abddf0934ffba0b6f7b89c57700676d
kouch-chivorn <kouchchivorn5@yahoo.com>
2017-10-25 07:27:59 +0700

    Update Devise User

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users, path: '', path_names: {sign_up: 'register', sign_in: 'login', sign_out: 'logout'}
   resources :recipes
   root "recipes#index"
 end
